e0a2c9f9395547e0caaa6c35e7a76b8a8615fbe4
Laurence Zanotti <lau_zanotti@hotmail.com>
2021-01-15 20:02:23 -0300

    Changed  temporary file session to default static folder

--- application.py
+++ application.py
@@ -55,7 +55,7 @@ def after_request(response):
     return response
 
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
+# app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
